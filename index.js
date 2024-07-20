ec2911978fd44ea70bd878d19259be18f018773b
Jonathan Fuentes <john.fuen95@gmail.com>
2024-07-20 00:01:29 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 const path = require('path');
+const redditData = require('./data.json')
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
@@ -9,21 +10,26 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/r/:subreddit', (req,res) => {
-    const {subreddit} = req.params;
-    res.render('subreddit', {subreddit})
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    const data = redditData[subreddit]
+    if (data) {
+        res.render('subreddit', { ...data })
+    } else {
+        res.send(`${subreddit.toUpperCase()} page does not exsist!`)
+    }
 })
 
-app.get('/rand', (req,res) => {
+app.get('/rand', (req, res) => {
     const num = Math.floor(Math.random() * 10) + 1
-    res.render('rand',  {num})
+    res.render('rand', { num })
 })
 
-app.get('/cats', (req,res) =>{
-   const cats = [
-    'Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston'
-   ] 
-   res.render('cats', {cats})
+app.get('/cats', (req, res) => {
+    const cats = [
+        'Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston'
+    ]
+    res.render('cats', { cats })
 })
 
 app.listen(3000, () => {
